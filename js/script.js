49f73472b4a344a3f317069ac47a5bf1b3f192e7
AlessioNapoletano <alessio.napoletano.job@gmail.com>
2022-12-12 15:27:02 +0100

    add H1

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const { createApp } = Vue
+    
+      createApp({
+        data() {
+          return {
+            message: 'Titolo h1, tramite VUE'
+          }
+        }
+      }).mount('#app')
